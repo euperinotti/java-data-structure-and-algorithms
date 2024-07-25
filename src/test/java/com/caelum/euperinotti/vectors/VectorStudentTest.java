dfd6bf69dd24060764badf12ed6aa3dd6fd453e0
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-07-25 15:15:10 -0300

    test: should add student at specific position

--- src/test/java/com/caelum/euperinotti/vectors/VectorStudentTest.java
+++ src/test/java/com/caelum/euperinotti/vectors/VectorStudentTest.java
@@ -18,4 +18,20 @@ public class VectorStudentTest {
     assertEquals(student, vector.get(0));
     assertEquals(student2, vector.get(1));
   }
+
+  @Test
+  public void shouldAddStudentAtSpecificPosition() {
+    Student student = new Student("Jonathan Hogan");
+    Student student2 = new Student("Jay Franklin");
+    Student student3 = new Student("Joe Stanley");
+
+    VectorStudent vector = new VectorStudent();
+
+    vector.add(student);
+    vector.add(0, student2);
+    vector.add(1, student3);
+
+    assertEquals(student2, vector.get(0));
+    assertEquals(student, vector.get(1));
+  }
 }

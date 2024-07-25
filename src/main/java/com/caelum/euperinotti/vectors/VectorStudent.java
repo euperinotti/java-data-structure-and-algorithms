300cce510d7c23f84a4a2a0485d842d4c97a9666
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-07-25 15:14:27 -0300

    feat: add vector student class

---
+++ src/main/java/com/caelum/euperinotti/vectors/VectorStudent.java
@@ -0,0 +1,41 @@
+package com.caelum.euperinotti.vectors;
+
+public class VectorStudent implements IVector<Student> {
+
+  @Override
+  public void add(Student o) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'add'");
+  }
+
+  @Override
+  public void add(int position, Student o) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'add'");
+  }
+
+  @Override
+  public Student get(int position) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'get'");
+  }
+
+  @Override
+  public void remove(int position) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'remove'");
+  }
+
+  @Override
+  public boolean contains(Student o) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'contains'");
+  }
+
+  @Override
+  public int size() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'size'");
+  }
+
+}

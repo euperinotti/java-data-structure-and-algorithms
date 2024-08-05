578c04969b7804127e2131a2303730a74d6c9346
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-05 12:53:08 -0300

    feat: add methods to linked list interface

--- src/main/java/com/caelum/euperinotti/linkedlist/ILinkedList.java
+++ src/main/java/com/caelum/euperinotti/linkedlist/ILinkedList.java
@@ -3,8 +3,11 @@ package com.caelum.euperinotti.linkedlist;
 public interface ILinkedList<T> {
   void add(T o);
   void add(int position, T o);
+  void addOnBeginning(T o);
   T get(int position);
   void remove(int position);
+  void removeFromBeginning();
+  void removeFromEnd();
   boolean contains(T o);
   int size();
 }

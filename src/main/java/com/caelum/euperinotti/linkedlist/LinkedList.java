7e1fc6ccb5f75d36e5b8cef71707df2c9e41074f
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-05 12:56:52 -0300

    feat: add linked list data structure

---
+++ src/main/java/com/caelum/euperinotti/linkedlist/LinkedList.java
@@ -0,0 +1,52 @@
+package com.caelum.euperinotti.linkedlist;
+
+public class LinkedList<T> implements ILinkedList<T>{
+  private Cell<T> first;
+  private Cell<T> last;
+
+  @Override
+  public void add(T o) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'add'");
+  }
+  @Override
+  public void add(int position, T o) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'add'");
+  }
+  @Override
+  public T get(int position) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'get'");
+  }
+  @Override
+  public void remove(int position) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'remove'");
+  }
+  @Override
+  public boolean contains(T o) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'contains'");
+  }
+  @Override
+  public int size() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'size'");
+  }
+  @Override
+  public void addOnBeginning(T o) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'addOnBeginning'");
+  }
+  @Override
+  public void removeFromBeginning() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'removeFromBeginning'");
+  }
+  @Override
+  public void removeFromEnd() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'removeFromEnd'");
+  }
+}

35e96c9aff24f322e8d8fbea032defe32e089fdb
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-10 22:29:39 -0300

    feat: add bubble sort implementation

--- src/main/java/com/caelum/euperinotti/algorithms/sort/bubblesort/BubbleSort.java
+++ src/main/java/com/caelum/euperinotti/algorithms/sort/bubblesort/BubbleSort.java
@@ -2,4 +2,17 @@ package com.caelum.euperinotti.algorithms.sort.bubblesort;
 
 public class BubbleSort {
 
+  public static int[] main(int[] arr) {
+    for (int i = 0; i < arr.length - 1; i++) {
+      for (int j = 0; j < arr.length - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          int temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+
+    return arr;
+  }
 }
---
+++ src/main/java/com/caelum/euperinotti/algorithms/sort/bubblesort/README.md
@@ -0,0 +1,8 @@
+# Bubble Sort
+
+Bubble sort is a sort algorithm that compares elements within an array and check if they are in order. If they are not, then they are switched and the loop goes on until the array is ordered.
+
+## Time complexity
+
+This algorithm has a time complexity of `O(n^2)`.
+The larger the array, the more time it will take to order, that's why this algorithm is not indicated for large arrays.

5996e518e20c63838118bc904f7e34221a14ac87
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-15 12:43:25 -0300

    feat: add push method implementation on stack

---
+++ src/main/java/com/caelum/euperinotti/datastructures/stack/Stack.java
@@ -0,0 +1,32 @@
+package com.caelum.euperinotti.datastructures.stack;
+
+import java.util.LinkedList;
+
+public class Stack<T> implements IStack<T> {
+
+  private LinkedList<T> stack = new LinkedList<T>();
+
+  @Override
+  public void push(T o) {
+    this.stack.add(o);
+  }
+
+  @Override
+  public T pop() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'pop'");
+  }
+
+  @Override
+  public boolean isEmpty() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+  }
+
+  @Override
+  public T peek() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'peek'");
+  }
+
+}

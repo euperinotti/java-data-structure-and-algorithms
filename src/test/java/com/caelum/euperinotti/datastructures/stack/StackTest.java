3d8937dd0d8a95411aa958e70b3c1b5624cbaefc
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-08-15 12:53:14 -0300

    test: add test cases for stack

--- src/test/java/com/caelum/euperinotti/datastructures/stack/StackTest.java
+++ src/test/java/com/caelum/euperinotti/datastructures/stack/StackTest.java
@@ -1,6 +1,10 @@
 package com.caelum.euperinotti.datastructures.stack;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import com.caelum.euperinotti.models.Piece;
 
@@ -11,5 +15,39 @@ public class StackTest {
   @BeforeEach
   void setUp() {
     this.sut = new Stack<Piece>();
+  }
+
+  @Test
+  void push_shouldPushObjectToStack() {
+    sut.push(new Piece("Sword"));
+
+    assertFalse(sut.isEmpty());
+  }
+
+  @Test
+  void pop_shouldRemoveObjectToStack() {
+    Piece piece = new Piece("Sword");
+    Piece piece2 = new Piece("Phone");
+
+    sut.push(piece);
+    sut.push(piece2);
+
+    assertTrue(piece2.equals(sut.pop()));
+  }
+
+  @Test
+  void peek_shouldReturnTopElementOfStack() {
+    Piece piece = new Piece("Sword");
+    Piece piece2 = new Piece("Phone");
+
+    sut.push(piece);
+    sut.push(piece2);
+
+    assertTrue(piece2.equals(sut.peek()));
+  }
+
+  @Test
+  void isEmpty_shouldReturnTrueIfStackIsEmpty() {
+    assertTrue(sut.isEmpty());
   }
 }
